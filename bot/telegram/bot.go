a48aa8b8112262bdbad9fd021e1e5f5648cd3363
Alexandre Moraes <alexandre.moraes@mercadolivre.com>
2023-02-17 18:27:07 -0300

    Implementing telegram bot

--- bot/telegram/bot.go
+++ bot/telegram/bot.go
@@ -13,6 +13,7 @@ import (
 type TelegramBot struct {
 	bot    *tele.Bot
 	router *claro.ClaroRouter
+	authGroup map[int64]bool
 }
 
 func (t *TelegramBot) Start() {
@@ -30,14 +31,41 @@ func (t *TelegramBot) Start() {
 		return
 	}
 
+	// Login to bot
+	b.Handle("/login", t.Login)
 	// Authenticate (or re-authenticate) your session in the router
-	b.Handle("/auth", t.AuthRouter)
+	b.Handle("/reauth", t.AuthRouter, t.LockMiddleware)
 	// Block a device by mac
-	b.Handle("/block", t.FilterMac)
+	b.Handle("/block", t.FilterMac, t.LockMiddleware)
 	// Unblocks a device by mac
-	b.Handle("/unblock", t.UnfilterMac)
+	b.Handle("/unblock", t.UnfilterMac, t.LockMiddleware)
 
 	b.Start()
+}
+
+func (t *TelegramBot) Login(c tele.Context) error {
+	arguments := c.Args()
+	if len(arguments) == 0 {
+		return c.Send("Please use /login [PASSWORD] to authenticate")
+	}
+	if arguments[0] == viper.GetString("telegram.password") {
+		t.authGroup[c.Sender().ID] = true
+		return c.Send("Login successful")
+	} else {
+		return c.Send("Wrong password")
+	}
+}
+
+func (t *TelegramBot) LockMiddleware(next tele.HandlerFunc) tele.HandlerFunc {
+	return func(c tele.Context) error {
+		if c.Callback() != nil {
+			defer c.Respond()
+		}
+		if t.authGroup[c.Sender().ID] {
+			return next(c) // continue execution chain
+		}
+		return c.Send("You are not authenticated. Please use /login [PASSWORD] to authenticate")
+	}
 }
 
 func (t *TelegramBot) AuthRouter(c tele.Context) error {
@@ -59,13 +87,18 @@ func (t *TelegramBot) FilterMac(c tele.Context) error {
 
 	for _, d := range devices {
 		btn := menu.Text(fmt.Sprintf("%s (%s)", d.MacAddress, d.Name))
-		t.bot.Handle(&btn, func(c tele.Context) error {
-			if err := t.router.FilterDeviceByMac(d.MacAddress); err != nil {
-				return c.Send("Failed to block the device")
-			} else {
-				return c.Send("Device blocked successfully")
-			}
-		})
+		
+		func (b *tele.Btn, m string) {
+			t.bot.Handle(b, func(c tele.Context) error {
+				fmt.Println("Blocking device: ", m)
+				if err := t.router.FilterDeviceByMac(m); err != nil {
+					return c.Send("Failed to block the device")
+				} else {
+					return c.Send("Device blocked successfully")
+				}
+			})
+		}(&btn, d.MacAddress)
+
 		options = append(options, menu.Row(btn))
 	}
 	menu.Reply(options...)
@@ -85,13 +118,16 @@ func (t *TelegramBot) UnfilterMac(c tele.Context) error {
 
 	for _, d := range devices {
 		btn := menu.Text(d.MacAddress)
-		t.bot.Handle(&btn, func(c tele.Context) error {
-			if err := t.router.UnfilterDeviceByMac(d.MacAddress); err != nil {
-				return c.Send("Failed to unblock the device")
-			} else {
-				return c.Send("Device unblocked successfully")
-			}
-		})
+		func (b *tele.Btn, m string) {
+			t.bot.Handle(b, func(c tele.Context) error {
+				fmt.Println("Unblocking device: ", m)
+				if err := t.router.UnfilterDeviceByMac(m); err != nil {
+					return c.Send("Failed to unblock the device")
+				} else {
+					return c.Send("Device unblocked successfully")
+				}
+			})
+		}(&btn, d.MacAddress)
 		options = append(options, menu.Row(btn))
 	}
 	menu.Reply(options...)
@@ -102,5 +138,6 @@ func (t *TelegramBot) UnfilterMac(c tele.Context) error {
 func NewTelegramBot() *TelegramBot {
 	return &TelegramBot{
 		router: claro.NewClaroRouter(),
+		authGroup: make(map[int64]bool, 0),
 	}
 }
